daf048c06ed79f7d35bb8ce74c1b247938d286f2
Winston Walter <winstonwalter@Winstons-Air.home>
2022-06-29 10:54:57 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)
+
+
+
+# Python CLI tool for Express app set up
+
+As the title says, this is a python CLI tool that makes boiler plate
+for an Express app.
+
+
+To use the tool, run the file in your command prompt
+
+```
+python qol.py <command> <true/false>
+
+```
+# Commands
+The commands are ```express-app``` and ```express-web-host```
+you can also type ```--help``` to get that list and with a brief 
+description.
+
+# True/False Options
+
+After the command, enter ```true``` if you want the file to be a 
+typescript file (.ts) or false if you want it to be a javascript
+file (.js)
+
+
+
+## Authors
+
+- [@blank2121](https://www.github.com/blank2121)
---
+++ qol.py
@@ -0,0 +1,142 @@
+import typer
+import os
+
+app = typer.Typer()
+
+@app.command(name="express-app")
+def express_setup(typescript: bool):
+    '''makes express app with get and post routes'''
+    script = '''const express = require("express"); 
+const app = express();
+
+app.set('view engine', /*enter a view engine, ejs is already installed*/);
+
+app.get("/", (req, res) => {
+    //put code in here
+
+    //send file
+    res.sendFile(/*file dir here*/);
+});
+
+app.post("/", (req, res) => {
+    res.json({/*json data here*/})
+});
+
+app.listen(/*port*/);'''
+
+
+    os.system("npm init -y")
+    os.system("npm i express.js")
+    os.system("npm install ejs")
+    os.system("mkdir src")
+    
+    if typescript:
+        with open("./src/index.ts", "w+") as f:
+            f.write(script)
+    else:
+        with open("./src/index.js", "w+") as f:
+            f.write(script)
+
+@app.command(name="express-web-host")
+def express_html(typescript: bool):
+    '''makes express app to host website'''
+
+    os.system("npm init -y")
+    os.system("npm i express.js")
+    os.system("npm i path.js")
+    os.system("npm i ejs")
+    os.system("mkdir src")
+    os.system("mkdir static")
+    os.system("mkdir views")
+    os.mkdir("./views/styles/")
+    
+    with open("./views/styles/style.css", "w+") as f:
+        f.write("""* {
+    padding: 0;
+    margin: 0;
+}
+
+h1 {
+    color: blue;
+}""")
+
+    with open("./views/home.html", "w+") as f:
+        content = '''<!--
+this is an example file of where your html file(s) will be.
+this also shows how you should link your css files.
+also make sure to use the static file for images and other things
+that will be static i.e. photos
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="./styles/style.css">
+    
+</head>
+<body>
+    <h1>Example Thing</h1>
+</body>
+</html>'''
+        f.write(content)
+
+    if typescript:
+        with open("./src/index.ts", "w+") as f:
+            content='''const express = require('express');
+const path = require('path');
+
+const app = express();
+
+//enter in the full directory to the project file
+
+//note do not type a slash at the end of the path
+const dir = "/Users/winstonwalter/Desktop/python-cli-tool/express-app-test";
+
+
+app.use(express.static(path.join(dir + "/views")))
+
+//setting a view engine is optional if you just use sendFile but it doesn't hurt to have it
+app.set('view engine', /*set a default view engine. "ejs" is preinstalled*/);
+
+
+app.get("/", (req, res) => {
+    //anytime you do sendFile, make sure you start with (dir + "/views/"+ <your html file in /views/>)
+    res.sendFile(dir + "/views/" + "home.html");
+});
+
+app.listen(/*port*/);'''
+            f.write(content)
+    else:
+        with open("./src/index.js", "w+") as f:
+            content = '''const express = require('express');
+const path = require('path');
+
+const app = express();
+
+//enter in the full directory to the project file
+
+//note do not type a slash at the end of the path
+const dir = "/Users/winstonwalter/Desktop/python-cli-tool/express-app-test";
+
+
+app.use(express.static(path.join(dir + "/views")))
+
+//setting a view engine is optional if you just use sendFile but it doesn't hurt to have it
+app.set('view engine', /*set a default view engine. "ejs" is preinstalled*/);
+
+
+app.get("/", (req, res) => {
+    //anytime you do sendFile, make sure you start with (dir + "/views/"+ <your html file in /views/>)
+    res.sendFile(dir + "/views/" + "home.html");
+});
+
+app.listen(/*port*/);'''
+            f.write(content)
+
+if __name__ == '__main__':
+    print("\n")
+    app()
